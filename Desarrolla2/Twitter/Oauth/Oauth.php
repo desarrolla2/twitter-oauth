28980cbe2b380feb011af9820ab616e0cbb55a71
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2012-11-12 01:03:34 +0100

    first commit

---
+++ Desarrolla2/Twitter/Oauth/Oauth.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the twitter-oauth proyect.
+ * 
+ * Copyright (c)
+ * Daniel González <daniel.gonzalez@freelancemadrid.es> 
+ * 
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ */
+namespace Desarrolla2\Twitter\Oauth;
+
+/**
+ * 
+ * Description of Oauth
+ *
+ * @author : Daniel González <daniel.gonzalez@freelancemadrid.es> 
+ * @file : Oauth.php , UTF-8
+ * @date : Nov 12, 2012 , 1:01:24 AM
+ */
+class Oauth
+{
+    //put your code here
+}
